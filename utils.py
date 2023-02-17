55da1abb8b9ed72e723968cd1e3865770d2ac45f
Alejandro Sierra <alejandro.sierra@hpi.de>
2023-02-17 18:13:31 +0100

    Fixed error with word_ids skipping

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Callable, Dict, List
+from typing import Callable, Dict, List, Optional, Tuple
 import numpy as np
 import re
 
@@ -21,24 +21,29 @@ def get_connectivity_matrix(word_ids:List[int]) -> np.ndarray:
     is_subword= lambda x: word_ids.count(word_ids[x])>1
     is_first_subword= lambda x: x==0 or word_ids[x]!=word_ids[x-1]
     for i,wid in enumerate(word_ids):
+        if is_first_subword(i):
+            if wid>0:
+                prev_w=word_ids.index(word_ids[i-1])
+                connectivity_m[i,prev_w]=1
+            if wid<max(word_ids):
+                ii=i+1
+                while ii<len(word_ids) and word_ids[ii]==wid:
+                    ii+=1
+                connectivity_m[i,ii]=1
         if is_subword(i):
             other_tokens_sw=[j for j,w in enumerate(word_ids) if w==wid and j!=i]
             for j in other_tokens_sw:
                 connectivity_m[i,j]=1
-            if is_first_subword(i):
-                if wid>0:
-                    prev_w=word_ids.index(wid-1)
-                    connectivity_m[i,prev_w]=1
-                if wid+1<=max(word_ids):    
-                    next_w=word_ids.index(wid+1)
-                    connectivity_m[i,next_w]=1
-        else:
-            if wid>0:
-                prev_w=word_ids.index(wid-1)
-                connectivity_m[i,prev_w]=1
-            if wid+1<=max(word_ids):    
-                next_w=word_ids.index(wid+1)
-                connectivity_m[i,next_w]=1
+            
+        # else:
+        #     if wid>0:
+        #         prev_w=word_ids.index(word_ids[i-1])
+        #         connectivity_m[i,prev_w]=1
+        #     if wid<max(word_ids):
+        #         ii=i+1
+        #         while ii<len(word_ids) and word_ids[ii]==wid:
+        #             ii+=1
+        #         connectivity_m[i,ii]=1
     return connectivity_m
 
 regex_extract_type=re.compile(r"[B,I]-(.*)")
