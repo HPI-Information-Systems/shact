30ff6712fcd50326f366e1ecfadd9092855ffe8e
Alejandro Sierra <Alejandro.Sierra@hpi.de>
2023-09-14 09:59:50 +0200

    Refactor adapter

--- data_adapters.py
+++ data_adapters.py
@@ -23,31 +23,48 @@ class Sentence:
         return f"Sentence(id={self.id}, text={self.text}, spans={self.spans})"
 
 #conll03 converter based on huggingface's conll03.py (id (string)	tokens (sequence)	pos_tags (sequence)	chunk_tags (sequence)	ner_tags (sequence))
-def conll03_ner_adapter(source_dataset:DatasetDict) -> DatasetDict:
+def convert_conll_ner_dataset(source_dataset: DatasetDict) -> DatasetDict:
     """
-    Converts a conll03 dataset to the normalized format.
+    Converts a NER dataset to the normalized format.
     """
-    class_label_map:ClassLabel = source_dataset["train"].features["ner_tags"].feature
-    #only keep the O and the class names from the B- tags
-    new_class_label_map:ClassLabel = ClassLabel(names=(["O"] + [label[2:] for label in class_label_map.names if label.startswith("B-")])) 
-    def map_iob_to_spans(iob_sequences:List[List[int]]):
+    feature = "ner_tags"
+    return convert_iob_dataset(source_dataset, feature)
+
+def convert_conll_chunk_dataset(source_dataset: DatasetDict) -> DatasetDict:
+    """
+    Converts a Chunk dataset to the normalized format.
+    """
+    feature = "chunk_tags"
+    return convert_iob_dataset(source_dataset, feature)
+
+def convert_iob_dataset(source_dataset: DatasetDict, feature: str) -> DatasetDict:
+    """
+    Converts a dataset to the normalized format.
+    """
+    class_label_map: ClassLabel = source_dataset["train"].features[feature].feature
+    # Only keep the O and the class names from the B- tags
+    new_class_label_map: ClassLabel = ClassLabel(
+        names=(["O"] + [label[2:] for label in class_label_map.names if label.startswith("B-")])
+    )
+
+    def map_iob_to_spans(iob_sequences: List[List[int]]):
         all_spans = []
         for iob_sequence in iob_sequences:
             spans = []
             current_entity = None
             for i, int_label in enumerate(iob_sequence):
                 str_label = class_label_map.int2str(int_label)
-                # check if label is an entity
+                # Check if label is an entity
                 if str_label.startswith("B-"):
-                    # check if there is an entity
+                    # Check if there is an entity
                     if current_entity:
                         spans.append(current_entity)
-                    current_entity = {"start":i, "end":i, "label":new_class_label_map.str2int(str_label[2:])}
+                    current_entity = {"start": i, "end": i, "label": new_class_label_map.str2int(str_label[2:])}
                 elif str_label.startswith("I-"):
                     if current_entity:
                         current_entity["end"] = i
                     else:
-                        current_entity = {"start":i, "end":i, "label":new_class_label_map.str2int(str_label[2:])}
+                        current_entity = {"start": i, "end": i, "label": new_class_label_map.str2int(str_label[2:])}
                 else:
                     if current_entity:
                         spans.append(current_entity)
@@ -57,64 +74,22 @@ def conll03_ner_adapter(source_dataset:DatasetDict) -> DatasetDict:
             all_spans.append(spans)
 
         return all_spans
-    # create mapping
+
+    # Create mapping
     def map_batch(batch):
-        spans=[]
-        return {"id":batch["id"], "text":batch["tokens"], "spans":map_iob_to_spans(batch["ner_tags"])}
-    
+        return {"id": batch["id"], "text": batch["tokens"], "spans": map_iob_to_spans(batch[feature])}
+
     target_dataset = DatasetDict()
 
     for split, dataset in source_dataset.items():
-        target_dataset[split] = dataset.map(map_batch, batched=True, remove_columns=["tokens","pos_tags", "chunk_tags", "ner_tags"],  desc="Mapping to normalized format", keep_in_memory=True)
-        target_dataset[split].features["spans"] = Sequence(feature={"start": Value("int32"), "end": Value("int32"), "label": new_class_label_map})
-
-    return target_dataset
-
-#conll03 converter based on huggingface's conll03.py (id (string)	tokens (sequence)	pos_tags (sequence)	chunk_tags (sequence)	ner_tags (sequence))
-def conll03_chunk_adapter(source_dataset:DatasetDict) -> DatasetDict:
-    """
-    Converts a conll03 dataset to the normalized format.
-    """
-    feature="chunk_tags"
-    class_label_map:ClassLabel = source_dataset["train"].features[feature].feature
-    #only keep the O and the class names from the B- tags
-    new_class_label_map:ClassLabel = ClassLabel(names=(["O"] + [label[2:] for label in class_label_map.names if label.startswith("B-")])) 
-    def map_iob_to_spans(iob_sequences:List[List[int]]):
-        all_spans = []
-        for iob_sequence in iob_sequences:
-            spans = []
-            current_entity = None
-            for i, int_label in enumerate(iob_sequence):
-                str_label = class_label_map.int2str(int_label)
-                # check if label is an entity
-                if str_label.startswith("B-"):
-                    # check if there is an entity
-                    if current_entity:
-                        spans.append(current_entity)
-                    current_entity = {"start":i, "end":i, "label":new_class_label_map.str2int(str_label[2:])}
-                elif str_label.startswith("I-"):
-                    if current_entity:
-                        current_entity["end"] = i
-                    else:
-                        current_entity = {"start":i, "end":i, "label":new_class_label_map.str2int(str_label[2:])}
-                else:
-                    if current_entity:
-                        spans.append(current_entity)
-                        current_entity = None
-            if current_entity:
-                spans.append(current_entity)
-            all_spans.append(spans)
-
-        return all_spans
-    # create mapping
-    def map_batch(batch):
-        spans=[]
-        return {"id":batch["id"], "text":batch["tokens"], "spans":map_iob_to_spans(batch[feature])}
-    
-    target_dataset = DatasetDict()
-
-    for split, dataset in source_dataset.items():
-        target_dataset[split] = dataset.map(map_batch, batched=True, remove_columns=["tokens","pos_tags", "chunk_tags", "ner_tags"],  desc="Mapping to normalized format", keep_in_memory=True)
-        target_dataset[split].features["spans"] = Sequence(feature={"start": Value("int32"), "end": Value("int32"), "label": new_class_label_map})
+        # list all columns that shoudl not be in the output
+        columns_to_remove = [col for col in source_dataset["train"].column_names if col not in ["id", "text", "spans"]]
+        target_dataset[split] = dataset.map(
+            map_batch, batched=True, remove_columns=columns_to_remove,
+            desc="Mapping to normalized format", keep_in_memory=True
+        )
+        target_dataset[split].features["spans"] = Sequence(
+            feature={"start": Value("int32"), "end": Value("int32"), "label": new_class_label_map}
+        )
 
     return target_dataset
