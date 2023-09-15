02b29c53b71c8ee1cdd027bc28b271e88da2d882
Alejandro Sierra <Alejandro.Sierra@hpi.de>
2023-09-15 17:12:19 +0200

    Genia adapter

--- data_adapters.py
+++ data_adapters.py
@@ -89,3 +89,52 @@ def convert_conll00_chunk_dataset(source_dataset: DatasetDict) -> DatasetDict:
     new_dict["train"]=split_dict["train"]
     new_dict["validation"]=split_dict["test"]
     return new_dict
+
+def convert_genia_dataset(source_dataset: DatasetDict) -> DatasetDict:
+    """
+    Converts a Genia dataset to the normalized format.
+    """
+    target_dataset=DatasetDict()
+    entity_types=["O"]
+    all_entities=source_dataset["train"]["entities"]
+    #build a set of all entity types efficiently
+    for entities in all_entities:
+        for entity in entities:
+            if entity["type"] not in entity_types:
+                entity_types.append(entity["type"])
+    entity_types
+    #build ClassLabel from entity types
+    class_label_obj: ClassLabel = ClassLabel(names=list(entity_types))
+    split_sizes=[len(source_dataset["train"]),len(source_dataset["validation"]),len(source_dataset["test"])]
+    #cumulative sum
+    split_sizes=[sum(split_sizes[:i+1]) for i in range(len(split_sizes))]
+    #build an id column for each split
+    #get the ranges
+    init=[0]+split_sizes[:-1]
+    ranges=[range(init[i],split_sizes[i]) for i in range(len(split_sizes))]
+    #build the id column
+    for i,split in enumerate(["train","validation","test"]):
+        target_dataset[split]=source_dataset[split].add_column("id",[str(i) for i in ranges[i]])
+    def map_entity(all_entities:List[List[dict]]):
+        all_spans=[]
+        for entities in all_entities:
+            spans=[]
+            for entity in entities:
+                spans.append({"start":entity["start"], "end":entity["end"], "label":class_label_obj.str2int(entity["type"])})
+            all_spans.append(spans)
+        return all_spans
+    def map_batch(batch):
+        return {"id": batch["id"], "text": batch["tokens"], "spans": map_entity(batch["entities"])}
+    for split in ["train","validation","test"]:
+        #build the spans column using map
+        columns_to_remove = [col for col in source_dataset["train"].column_names if col not in ["id", "text", "spans"]]
+        target_dataset[split]=target_dataset[split].map(map_batch,batched=True, remove_columns=columns_to_remove,
+            desc="Mapping to normalized format", keep_in_memory=True,)
+        features=target_dataset[split].features.copy()
+        features["spans"]=[{"start": Value(dtype="int32"), "end": Value(dtype="int32"), "label": class_label_obj}]
+        target_dataset[split]=target_dataset[split].cast(features)
+    return target_dataset
+        
+        
+
+
